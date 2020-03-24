4ec18222acba755398fa5378a6099674e4eda27c
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-03-24 17:45:53 -0600

    added proxy files and establishing routes for other services
     Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = requre('cors')
 const app = express();
-
-
 const PORT = process.env.PORT || 3000;
+
+
+
+app.use(cors());
+app.use(morgan());
+
+
+
+
 
 
 
